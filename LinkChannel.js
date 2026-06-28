6b4021f39f8f1f71ee3dce776af42179939130b6
TheRudyPlayer <elerandomxd25@outlook.com>
2026-06-28 19:13:42 -0400

    Add CGTubeChannels array with channel data

---
+++ LinkChannel.js
@@ -0,0 +1,18 @@
+const CGTubeChannels = [
+
+{
+    id: "UC34MyNlsyxD4DV_rtzTXsGg",
+    handle: "@elrudyplayeroficial"
+},
+
+{
+    id: "UC5aLmBd_aN34pcpjTxjvEPw",
+    handle: "@JrCrackOficial"
+},
+
+{
+    id: "UCyyyyyyyyyyyyyyyyyyyyyy",
+    handle: "@Canal3"
+}
+
+];
