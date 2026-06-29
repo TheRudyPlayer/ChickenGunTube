c0240cda056fb836676a4a8f26a6cbb3e9bd6c40
TheRudyPlayer <elerandomxd25@outlook.com>
2026-06-28 20:42:11 -0400

    Add icon URLs for user profiles
    Added icon URLs for two users in LinkChannel.js.

--- LinkChannel.js
+++ LinkChannel.js
@@ -3,13 +3,15 @@ const CGTubeChannels = [
 {
     id: "UC34MyNlsyxD4DV_rtzTXsGg",
     handle: "@elrudyplayeroficial",
-    name: "TheRudyPlayer"
+    name: "TheRudyPlayer",
+    icon: "https://yt3.googleusercontent.com/27vaT2VdaJDa3QVxsdh9IH8O0A0aKrWp9VqGXdNbX6kQvrI7fb5t6AsPBET0G10RTJZkLSL_OB8=s160-c-k-c0x00ffffff-no-rj"
 },
 
 {
     id: "UC5aLmBd_aN34pcpjTxjvEPw",
     handle: "@JrCrackOficial",
-    name: "J.R Crack"
+    name: "J.R Crack",
+    icon: "https://yt3.googleusercontent.com/6La7cnBuqVzQX_Jpm4MgBiqdDayg5lWs2B4tBKYMN2JKBgWK_LTL0xTtWKcJW2Oy4sZtJDhqFA=s160-c-k-c0x00ffffff-no-rj"
 },
 
 {
